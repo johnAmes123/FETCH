d883dbe9bd85bf7665de49e3d7643f3e2fd6b021
johnAmes123 <johnnzsilva@gmail.com>
2020-02-24 10:09:32 +1300

    test
    test

--- WebApplication1/WebApplication1/custom.js
+++ WebApplication1/WebApplication1/custom.js
@@ -1,1 +1,22 @@
-﻿
+﻿const path = 'data.json';
+
+fetch(path).then(function (response) {
+    return response.json();
+
+}).then(function (data) {
+
+    console.log(data);
+
+    console.log(data.FirstName);
+
+    console.log(data.LastName);
+
+    document.getElementById("testData").innerHTML = data.FirstName;
+
+    console.log(Object.keys(data));
+
+    console.log(Object.values(data));
+
+
+});
+
