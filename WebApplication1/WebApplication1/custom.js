3144d42cd20a07e780f38f0cf0a287a0695ef57e
johnAmes123 <johnnzsilva@gmail.com>
2020-02-24 11:45:42 +1300

    fetch day1
    Fetch Day1

--- WebApplication1/WebApplication1/custom.js
+++ WebApplication1/WebApplication1/custom.js
@@ -1,6 +1,7 @@
 ﻿const path = 'data.json';
-
-var myRequest = new Request(path, myInit);
+const path1 = 'users.json';
+const chart = 'chart.json'; // JSON FILE
+var myRequest = new Request(chart, myInit);
 
 var myInit = {
     method: 'GET',
@@ -15,6 +16,69 @@ fetch(myRequest).then(function (response) {
 }).then(function (data) {
 
     console.log(data);
+    // ChartJS should go over here
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            // LOAD IT FROM A JSON
+            labels: data.labels,
+            datasets: [{
+                // LOAD FROM A JSON
+                label: data.dataSet1.label,
+                backgroundColor: data.color[0],
+                borderColor: data.color[0],
+                // LOAD FROM A JSON
+                data: data.dataSet1.data
+            },
+            {
+                // LOAD FROM A JSON
+                label: data.dataSet2.label,
+                backgroundColor: data.color[1],
+                borderColor: data.color[1],
+                // LOAD FROM A JSON
+                data: data.dataSet2.data
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            responsive: false
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    console.log(data);
+
+    console.log(data.User1.FirstName);
+
+    console.log(data.User1.Hobbies[0]);
+
+
+
+
+
+
+
+
 
 }).catch(function () {
     console.log("error!!");
